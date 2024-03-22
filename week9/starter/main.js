942be55c3fb554a9cc33ccda5e8d2b3aa0a9bc18
dela0331 <dela0331@algonquinlive.com>
2024-03-22 08:31:02 -0400

    changes

--- week9/starter/main.js
+++ week9/starter/main.js
@@ -3,18 +3,23 @@
 // 1.Display the ID of the last student in the list.
 const $studentList = document.getElementById('studentList')
 
+
+function displayStudentId(){
 $studentList.innerHTML = `<div class="col-md-6">
     <div class="ac-card">
         <div class="ac-card-image d-flex justify-content-center align-items-center"><i class="bi bi-person-bounding-box display-1"></i></div>
         <div class="ac-card-info">
             <p>
-            <strong>${students[0].name}</strong><br>
-            <span>${students[0].studentNumber}</span>-
-            <span>${students[0].userName}</span>
+            <strong>${students[students.length - 1].name}</strong><br>
+            <span>${students[students.length - 1].studentNumber}</span>-
+            <span>${students[students.length - 1].userName}</span>
             </p>
-            <p>${students[0].email}</p>
+            <p>${students[students.length - 1].email}</p>
+
             <!-- here the button -->
-            <button type="button" class="btn btn-primary">Update</button>
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Launch demo modal
+            </button>
            
         </div>
         <div class="ac-card-footer">
@@ -23,14 +28,39 @@ $studentList.innerHTML = `<div class="col-md-6">
     </div>
     </div>
     `
+}
+
+displayStudentId()
 // 2.Create a button in the student ID. Use the Bootstrap modal.
-
+const $button = document.querySelector('button')
 // 3.Add an event listener to the button.
-
-
+$button.addEventListener('click', click)
+function click(){
+    console.log("click")
+    $button.setAttribute('data-bs-toggle', 'modal')
+    $button.setAttribute('data-bs-target', '#exampleModal')
+}
 /*** Form ***/
 // 5.Add an event listener to the 'update form' to allow updating of the student's information.
+const $updateForm = document.getElementById('updateForm')
+$updateForm.addEventListener('submit', function(event){
+event.preventDefault()
+console.log($updateForm.elements)
 
+//
+for(const input of $updateForm.elements){
+    console.log(input.name)
+}
+
+const inputUsername = document.getElementById('inputUsername').value
+
+//updates student information
+students[students.length - 1].userName = inputUsername
+
+displayStudentId()
+})
 
 /*** Filter ***/
 // 6.Add an event listener to the 'Search' form. Test it with both 'submit' and 'input' events to display a filtered list based on the student's name.
+const search = document.getElementById('searchForm')
+
